15ed5ef24a886e1d9121a57d27b01f07e027b8ac
Giovanni Flores <giovannimoreiraflores40@gmail.com>
2020-06-15 22:35:01 -0300

    Springboot with h2 Database

---
+++ exemploh2database/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+//DML
+
+insert into cliente(id_cliente, nome, renda)
+values(1, 'Giovanni Flores', 300.20);
---
+++ exemploh2database/src/main/resources/schema.sql
@@ -0,0 +1,10 @@
+//DDl
+
+DROP TABLE IF EXISTS cliente;
+
+CREATE TABLE cliente (
+		 id_cliente BIGINT AUTO_INCREMENT NOT NULL,
+		 nome VARCHAR(255) NULL,
+		 renda DOUBLE NULL,
+		 PRIMARY KEY (id_cliente));
+
